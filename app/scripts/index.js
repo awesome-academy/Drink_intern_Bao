b391b141cbeb660db72e4129b6e809d043b7194e
thebaodev <nguyen.the.vinh.bao@framgia.com>
2019-04-04 21:26:43 +0700

    Created Homepage and components

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,3 +5,13 @@ import '@fortawesome/fontawesome-free/js/all';
 if (process.env.NODE_ENV !== 'production') {
     require('../index.pug');
 }
+
+// Toogle Top-Menu
+
+function classToggle() {
+    const navs = document.querySelectorAll('.menu__item');
+    navs.forEach(nav => nav.classList.toggle('h-toogle-nav-show'));
+}
+document
+    .querySelector('.top-menu__toggle')
+    .addEventListener('click', classToggle);
