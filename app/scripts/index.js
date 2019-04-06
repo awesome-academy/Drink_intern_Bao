7282c0351927d9a8d0e39b905a4865dec169c1ee
thebaodev <nguyen.the.vinh.bao@framgia.com>
2019-04-06 10:49:06 +0700

    Created Detail Page and components

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,11 @@
 import '../styles/main.scss';
 import 'bootstrap';
+
 import '@fortawesome/fontawesome-free/js/all';
 
 if (process.env.NODE_ENV !== 'production') {
     require('../index.pug');
+    require('../detail.pug');
 }
 
 // Toogle Top-Menu
